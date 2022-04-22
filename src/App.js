4da899dbbd406131fe4831883324b5487208cb95
ofa-abdullayeva <abdullayevaofa25@gmail.com>
2022-04-22 10:45:51 +0400

    UseState

--- src/App.js
+++ src/App.js
@@ -48,25 +48,37 @@ import './App.css';
 
 
 const Slider = (props) => {
-    const [slide, setSlide] = useState(0);
-    const [autoplay, setAutoPlay] = useState(false)
+    // const [slide, setSlide] = useState(0);
+    // const [autoplay, setAutoPlay] = useState(false)
+
+    // const changeSlide = (i) => {
+    //     setSlide(slide => slide + i)
+
+    // }
+
+    // const toggleAutoplay = () => {
+    //     setAutoPlay(autoplay => !autoplay)
+    // }
+    const [state,setState] = useState({
+        slide: 0,
+        autoplay: false
+    })
 
     const changeSlide = (i) => {
-        setSlide(slide=>slide + i)
-        setSlide(slide=>slide + i)
-        setSlide(slide=>slide + i)
-    }
+            setState({...state, slide: state.slide + i})
+    
+        }
 
-    const toggleAutoplay = () => {
-        setAutoPlay(!autoplay)
-    }
-
+        const toggleAutoplay = () => {
+            setState({...state, autoplay: !state.autoplay})
+    
+        }
 
     return (
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                <div className="text-center mt-5">Active slide {slide}</div>{autoplay ? 'auto' : null}
+                <div className="text-center mt-5">Active slide {state.slide}</div>{state.autoplay ? 'auto' : null}
                 <div className="buttons mt-3">
                     <button
                         className="btn btn-primary me-2"
